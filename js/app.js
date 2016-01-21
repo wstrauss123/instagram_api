e70f6293ff20064e6105039b1f4ba25ef7bbe884
wstrauss123 <wstrauss123@gmail.com>
2016-01-21 00:15:35 -0800

    update with reset and more pics

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
+        $("h2").click(function(){
+            location.reload();
+        });
+
         $.ajax({
         type: "GET",
         dataType: "jsonp",
         cache: false,
         url: "https://api.instagram.com/v1/media/popular?access_token=2703086432.1677ed0.6ce9d11dbacb415fa57028b51a74cff2",
         success: function(data) {
-            for (var i = 0; i < 7; i++) {
+            for (var i = 0; i < 20; i++) {
                 $("#space").append("<img src='" + data.data[i].images.low_resolution.url + "'></img></a>");
             }
         }
