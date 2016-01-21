ce96d181c7d97a5dfde4f93c45ad23be47c9c6c6
wstrauss123 <wstrauss123@gmail.com>
2016-01-20 23:57:37 -0800

    working now

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         type: "GET",
         dataType: "jsonp",
         cache: false,
-        url: "https://api.instagram.com/v1/media/popular?access_token=2061040706.2b508a9.5ccd0dd31dfd454f95b82792df8d9832",
+        url: "https://api.instagram.com/v1/media/popular?access_token=2703086432.1677ed0.6ce9d11dbacb415fa57028b51a74cff2",
         success: function(data) {
             for (var i = 0; i < 7; i++) {
                 $("#space").append("<img src='" + data.data[i].images.low_resolution.url + "'></img></a>");
